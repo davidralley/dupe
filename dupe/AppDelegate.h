b839d7da534c9c10ff092e500b1028c2b9496b28
david <david@stravinsky.local>
2015-10-10 16:45:51 -0400

    Initial Commit

---
+++ dupe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dupe
+//
+//  Created by david on 10/10/15.
+//  Copyright © 2015 Trance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ dupe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  dupe
+//
+//  Created by david on 10/10/15.
+//  Copyright © 2015 Trance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
